30ebf8423e669b650dfef8e313153f8a4a65740f
Uria <reapuria18@gmail.com>
2025-11-26 17:38:21 +0100

    pages + db update

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class RegisterController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/register', name: 'app_register')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('register/index.html.twig', [
+            'controller_name' => 'RegisterController',
         ]);
     }
 }
