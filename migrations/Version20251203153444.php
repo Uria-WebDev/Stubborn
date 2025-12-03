cb99afe2bdffe6b6c787734f8a6fe6be4b0aaa5c
Uria <reapuria18@gmail.com>
2025-12-03 17:48:24 +0100

    added product page + other pages improvement

--- migrations/Version20251203153444.php
+++ migrations/Version20251203153444.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251127162235 extends AbstractMigration
+final class Version20251203153444 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20251127162235 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD image VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE size ADD CONSTRAINT FK_F7C0246ADCD6110 FOREIGN KEY (stock_id) REFERENCES stock (id)');
+        $this->addSql('CREATE INDEX IDX_F7C0246ADCD6110 ON size (stock_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP image');
+        $this->addSql('ALTER TABLE size DROP FOREIGN KEY FK_F7C0246ADCD6110');
+        $this->addSql('DROP INDEX IDX_F7C0246ADCD6110 ON size');
     }
 }
